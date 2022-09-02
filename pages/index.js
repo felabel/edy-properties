ed5e3a6696636b6eeee5d5edb80aee574b1de889
felabel <felicityabel2016@gmail.com>
2022-09-02 23:50:12 +0100

    fixing vercel bugs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
 import Properties from '../components/property/Properties';
-import propertyData from './propertyData';
-// import PropertyCard from './PropertyCard';
-// import db from '../../../utils/db';
-import db from '../firebase';
+
 import Hero from '../components/Hero/Hero';
 import Map from '../components/Map/Map';
 import Layout from '../components/Layout';
